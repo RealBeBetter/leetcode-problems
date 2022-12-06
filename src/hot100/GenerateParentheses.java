c7431d36b4e63d944fe4c2903ad95d8f0c314179
Real <swrely@qq.com>
2022-12-06 19:51:11 +0800

    feat: init hot100——GenerateParentheses

---
+++ src/hot100/GenerateParentheses.java
@@ -0,0 +1,35 @@
+package hot100;
+
+import java.util.List;
+
+/**
+ * 22. 括号生成
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * <p>
+ * 示例 1：
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ * 示例 2：
+ * 输入：n = 1
+ * 输出：["()"]
+ * <p>
+ * 提示：
+ * 1 <= n <= 8
+ *
+ * @author wei.song
+ * @since 2022/12/6 19:48
+ */
+public class GenerateParentheses {
+
+    /**
+     * 括号生成，输入 n ，字符串一共包含 2n 个字符，(、) 各 n 个
+     * 使用暴力生成所有的括号，逐一判断是否含有目标括号切是否合法
+     *
+     * @param n 括号数
+     * @return List
+     */
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+
+}
