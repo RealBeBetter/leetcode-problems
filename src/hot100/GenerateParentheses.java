b5310e61e7e3f7b2542e1c01c3fdedc4064f16fb
Real <swrely@qq.com>
2022-12-07 00:23:28 +0800

    feat: add hot100——GenerateParentheses

--- src/hot100/GenerateParentheses.java
+++ src/hot100/GenerateParentheses.java
@@ -1,5 +1,6 @@
 package hot100;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,15 +22,37 @@ import java.util.List;
  */
 public class GenerateParentheses {
 
+    List<String> res = new ArrayList<>();
+
     /**
      * 括号生成，输入 n ，字符串一共包含 2n 个字符，(、) 各 n 个
      * 使用暴力生成所有的括号，逐一判断是否含有目标括号切是否合法
+     * <p>
+     * dfs 遍历生成目标括号
      *
      * @param n 括号数
      * @return List
      */
     public List<String> generateParenthesis(int n) {
-        return null;
+        dfs(n, n, "");
+        return res;
+    }
+
+    private void dfs(int left, int right, String curStr) {
+        if (left == 0 && right == 0) {
+            // 左右括号都不剩余了，递归终止
+            res.add(curStr);
+            return;
+        }
+
+        if (left > 0) {
+            // 如果左括号还剩余的话，可以拼接左括号
+            dfs(left - 1, right, curStr + "(");
+        }
+        if (right > left) {
+            // 如果右括号剩余多于左括号剩余的话，可以拼接右括号
+            dfs(left, right - 1, curStr + ")");
+        }
     }
 
 }
