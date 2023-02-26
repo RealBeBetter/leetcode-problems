b8084ba1bdc937471185e6191cc3a23e6a8c1471
Real <swrely@qq.com>
2023-02-26 14:56:56 +0800

    feat: add 334th weekly-competition

---
+++ src/weekly_competition/data2023_02/week334/FindDivisibilityArrayOfString.java
@@ -0,0 +1,61 @@
+package weekly_competition.data2023_02.week334;
+
+/**
+ * 6368. 找出字符串的可整除数组
+ * 给你一个下标从 0 开始的字符串 word ，长度为 n ，由从 0 到 9 的数字组成。另给你一个正整数 m 。
+ * word 的 可整除数组 div  是一个长度为 n 的整数数组，并满足：
+ * 如果 word[0,...,i] 所表示的 数值 能被 m 整除，div[i] = 1
+ * 否则，div[i] = 0
+ * 返回 word 的可整除数组。
+ * <p>
+ * 示例 1：
+ * 输入：word = "998244353", m = 3
+ * 输出：[1,1,0,0,0,1,1,0,0]
+ * 解释：仅有 4 个前缀可以被 3 整除："9"、"99"、"998244" 和 "9982443" 。
+ * 示例 2：
+ * 输入：word = "1010", m = 10
+ * 输出：[0,1,0,1]
+ * 解释：仅有 2 个前缀可以被 10 整除："10" 和 "1010" 。
+ * <p>
+ * 提示：
+ * 1 <= n <= 105
+ * word.length == n
+ * word 由数字 0 到 9 组成
+ * 1 <= m <= 109
+ *
+ * @author Real
+ * @since 2023/2/26 10:45
+ */
+public class FindDivisibilityArrayOfString {
+
+    public static void main(String[] args) {
+    }
+
+    /**
+     * 前缀能被给定数字整除的index数组
+     *
+     * @param word 字符串
+     * @param m    数字
+     * @return {@link int[]}
+     */
+    public static int[] divisibilityArray(String word, int m) {
+        if (word == null || word.length() == 0) {
+            return null;
+        }
+
+        int length = word.length();
+        int[] ans = new int[length];
+        long[] currentMod = new long[length + 1];
+        currentMod[0] = Integer.parseInt(String.valueOf(word.charAt(0))) % m;
+        ans[0] = currentMod[0] == 0 ? 1 : 0;
+
+        for (int i = 1; i < length; i++) {
+            long num = currentMod[i - 1] * 10 + Integer.parseInt(String.valueOf(word.charAt(i)));
+            currentMod[i] = num % m;
+            ans[i] = currentMod[i] == 0 ? 1 : 0;
+        }
+
+        return ans;
+    }
+
+}
---
+++ src/weekly_competition/data2023_02/week334/LeftAndRightSumDifferences.java
@@ -0,0 +1,70 @@
+package weekly_competition.data2023_02.week334;
+
+import java.util.Arrays;
+
+/**
+ * 6369. 左右元素和的差值
+ * 给你一个下标从 0 开始的整数数组 nums ，请你找出一个下标从 0 开始的整数数组 answer ，其中：
+ * answer.length == nums.length
+ * answer[i] = |leftSum[i] - rightSum[i]|
+ * 其中：
+ * leftSum[i] 是数组 nums 中下标 i 左侧元素之和。如果不存在对应的元素，leftSum[i] = 0 。
+ * rightSum[i] 是数组 nums 中下标 i 右侧元素之和。如果不存在对应的元素，rightSum[i] = 0 。
+ * 返回数组 answer 。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [10,4,8,3]
+ * 输出：[15,1,11,22]
+ * 解释：数组 leftSum 为 [0,10,14,22] 且数组 rightSum 为 [15,11,3,0] 。
+ * 数组 answer 为 [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22] 。
+ * 示例 2：
+ * 输入：nums = [1]
+ * 输出：[0]
+ * 解释：数组 leftSum 为 [0] 且数组 rightSum 为 [0] 。
+ * 数组 answer 为 [|0 - 0|] = [0] 。
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 1000
+ * 1 <= nums[i] <= 105
+ *
+ * @author Real
+ * @since 2023/2/26 10:32
+ */
+public class LeftAndRightSumDifferences {
+
+    /**
+     * 输出左侧元素和右侧元素差值的绝对值数组
+     *
+     * @param nums 数组
+     * @return {@link int[]}
+     */
+    public int[] leftRigthDifference(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        int length = nums.length;
+        int[] ans = new int[length];
+
+        long sum = Arrays.stream(nums).reduce(Integer::sum).getAsInt();
+        long currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum = calcSum(nums, i - 1);
+            long currentRightSum = sum - currentSum - nums[i];
+            ans[i] = (int) Math.abs(currentSum - currentRightSum);
+        }
+
+        return ans;
+    }
+
+    private long calcSum(int[] array, int end) {
+        int length = array.length;
+        long sum = 0;
+        end = Math.min(end, length - 1);
+        for (int i = 0; i <= end; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+}
