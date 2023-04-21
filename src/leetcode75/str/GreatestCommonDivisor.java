764f3c0078705dc3638802fbcde8bda799622b1e
Real <swrely@qq.com>
2023-04-21 23:35:17 +0800

    leetcode75: add str: GreatestCommonDivisor/MergeStringsAlternately

---
+++ src/leetcode75/str/GreatestCommonDivisor.java
@@ -0,0 +1,35 @@
+package leetcode75.str;
+
+/**
+ * 1071. 字符串的最大公因子
+ *
+ * @author Real
+ * @since 2023/4/21 23:10
+ */
+public class GreatestCommonDivisor {
+
+    public String gcdOfStrings(String str1, String str2) {
+        int len1 = str1.length(), len2 = str2.length();
+        // 从长度大的开始枚举
+        for (int i = Math.min(len1, len2); i >= 1; --i) {
+            if (len1 % i == 0 && len2 % i == 0) {
+                String sub = str1.substring(0, i);
+                if (check(str1, sub) && check(str2, sub)) {
+                    return sub;
+                }
+            }
+        }
+
+        return "";
+    }
+
+    public boolean check(String str, String sub) {
+        int num = str.length() / sub.length();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 1; i <= num; ++i) {
+            ans.append(sub);
+        }
+
+        return ans.toString().equals(str);
+    }
+}
---
+++ src/leetcode75/str/MergeStringsAlternately.java
@@ -0,0 +1,38 @@
+package leetcode75.str;
+
+/**
+ * 1768. 交替合并字符串
+ *
+ * @author Real
+ * @since 2023/4/21 23:01
+ */
+public class MergeStringsAlternately {
+
+    /**
+     * 从 word1 开始，通过交替添加字母来合并字符串
+     *
+     * @param word1 word1
+     * @param word2 word2
+     * @return {@link String}
+     */
+    public String mergeAlternately(String word1, String word2) {
+        int index1 = 0, index2 = 0;
+        int length1 = word1.length(), length2 = word2.length();
+
+        StringBuilder ans = new StringBuilder();
+
+        while (index1 < length1 && index2 < length2) {
+            ans.append(word1.charAt(index1++));
+            ans.append(word2.charAt(index2++));
+        }
+        if (index1 < length1) {
+            ans.append(word1.substring(index1));
+        }
+        if (index2 < length2) {
+            ans.append(word2.substring(index2));
+        }
+
+        return ans.toString();
+    }
+
+}
