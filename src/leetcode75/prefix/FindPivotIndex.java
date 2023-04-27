5dab120c8a7fc243ef4eb326e57d77e685ba8a3f
Real <swrely@qq.com>
2023-04-27 22:43:54 +0800

    feat: add leetcode75-FindPivotIndex

--- src/leetcode75/prefix/FindPivotIndex.java
+++ src/leetcode75/prefix/FindPivotIndex.java
@@ -1,6 +1,29 @@
-package leetcode75.prefix;/**
+package leetcode75.prefix;
+
+/**
+ * 724. 寻找数组的中心下标
+ *
  * @author Real
  * @since 2023/4/27 22:38
  */
 public class FindPivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        int[] prefix = new int[nums.length + 1];
+        int[] suffix = new int[nums.length + 1];
+
+        for (int i = 0, j = nums.length - 1; i < nums.length && j > 0; i++, j--) {
+            prefix[i + 1] = prefix[i] + nums[i];
+            suffix[j - 1] = suffix[j] + nums[j];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (prefix[i] == suffix[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
 }
