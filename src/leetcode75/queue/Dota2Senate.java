75eef5af6441f5904af243b240a30301ebd3836d
Real <swrely@qq.com>
2023-05-02 22:14:04 +0800

    feat: add leetcode75-Dota2Senate

--- src/leetcode75/queue/Dota2Senate.java
+++ src/leetcode75/queue/Dota2Senate.java
@@ -11,28 +11,76 @@ import java.util.Queue;
  */
 public class Dota2Senate {
 
+    public static void main(String[] args) {
+        Dota2Senate dota = new Dota2Senate();
+        System.out.println(dota.predictPartyVictory("RD"));
+        System.out.println(dota.predictPartyVictory("RDD"));
+    }
+
 
     /**
      * 预测一方胜利
+     * 关键因素：判断对方是否需要跳过。移除元素时，index 不需要变化。
      *
-     * @param senate 参议院
+     * @param senate 参议院名单
      * @return {@link String}
      */
     public String predictPartyVictory(String senate) {
         String dire = "Dire";
         String radiant = "Radiant";
 
-        Queue<Character> queue = new LinkedList<>();
-        int length = senate.length();
-        for (int i = 0; i < length; i++) {
-            if (senate.charAt(i) == dire.charAt(0)) {
-                queue.offer(dire.charAt(0));
+        int dSkipCount = 0;
+        int rSkipCount = 0;
+
+        LinkedList<Character> queue = new LinkedList<>();
+        for (int i = 0; i < senate.length(); i++) {
+            char c = senate.charAt(i);
+            queue.add(c);
+        }
+
+        Character direSymbol = dire.charAt(0);
+        int point = 0;
+        while (!queue.isEmpty() && !isSameParty(queue)) {
+            if (point == queue.size()) {
+                point = 0;
+            }
+            Character element = queue.get(point);
+            if (direSymbol.equals(element)) {
+                if (dSkipCount == 0) {
+                    rSkipCount++;
+                    point++;
+                } else {
+                    queue.remove(point);
+                    dSkipCount--;
+                }
             } else {
-                queue.offer(radiant.charAt(0));
+                if (rSkipCount == 0) {
+                    dSkipCount++;
+                    point++;
+                } else {
+                    queue.remove(point);
+                    rSkipCount--;
+                }
             }
         }
 
-        return null;
+        if (direSymbol.equals(queue.peek())) {
+            return dire;
+        }
+        return radiant;
     }
 
+    private boolean isSameParty(Queue<Character> queue) {
+        if (queue == null || queue.isEmpty()) {
+            return false;
+        }
+
+        Character first = queue.peek();
+        for (Character character : queue) {
+            if (!character.equals(first)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
