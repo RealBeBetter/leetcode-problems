9c7cd30b9f5b342d3a91dbb871dcf48f2401c981
wei.song <swrely@qq.com>
2023-08-18 00:32:23 +0800

    fix: fix project gitignore

---
+++ src/structure/stack/ArrayStack.java
@@ -0,0 +1,26 @@
+package structure.stack;
+
+/**
+ * @author wei.song
+ * @since 2023/8/18 0:27
+ */
+public class ArrayStack {
+
+    /**
+     * 保存栈中的元素数据
+     */
+    private String[] items;
+
+    /**
+     * 容量
+     */
+    private int capacity;
+
+    /**
+     * 栈当前的大小
+     */
+    private int size;
+
+
+
+}
