f0e1a2704ad5c24452d3149beb89b175add2e31d
wei.song <wei.song@anker-in.com>
2023-08-18 15:04:06 +0800

    feat: add structure stack

--- src/structure/stack/ArrayStack.java
+++ src/structure/stack/ArrayStack.java
@@ -21,6 +21,64 @@ public class ArrayStack {
      */
     private int size;
 
+    public ArrayStack(int capacity) {
+        this.capacity = capacity;
+        this.items = new String[capacity];
+        this.size = 0;
+    }
 
+    public void push(String item) {
+        if (size == capacity) {
+            return;
+        }
+
+        items[size++] = item;
+    }
+
+
+    public String pop() {
+        if (size == 0) {
+            return null;
+        }
+
+        String pop = items[--size];
+        items[size] = null;
+        return pop;
+    }
+
+    public String peek() {
+        if (size == 0) {
+            return null;
+        }
+
+        return items[size - 1];
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(5);
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.push("4");
+        stack.push("5");
+
+        System.out.println(stack.isFull());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
 
 }
