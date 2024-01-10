5c6c63e7a145def6a192d04bb8633b1c22703a76
wei.song <swrely@qq.com>
2024-01-11 00:35:33 +0800

    fix(data-structure): Add Bitmap Implementation

---
+++ src/structure/bitmap/BitMap.java
@@ -0,0 +1,35 @@
+package structure.bitmap;
+
+/**
+ * @author Real
+ * @since 2024/1/10 23:57
+ */
+public class BitMap {
+
+    private final char[] bytes;
+    private final int nbits;
+
+    public BitMap(int nbits) {
+        this.nbits = nbits;
+        this.bytes = new char[nbits / 16 + 1];
+    }
+
+    public void set(int k) {
+        if (k > nbits) {
+            return;
+        }
+        int byteIndex = k / 16;
+        int bitIndex = k % 16;
+        bytes[byteIndex] |= (char) (1 << bitIndex);
+    }
+
+    public boolean get(int k) {
+        if (k > nbits) {
+            return false;
+        }
+        int byteIndex = k / 16;
+        int bitIndex = k % 16;
+        return (bytes[byteIndex] & (1 << bitIndex)) != 0;
+    }
+
+}
